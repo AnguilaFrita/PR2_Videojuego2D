99ac8657a9e19df36106ce7a5c79bc6e19d7cf62
“AnguilaFrita” <disconected15@gmail.com>
2025-04-03 20:44:57 +0200

    script personaje: movimiento

--- Assets/scripts/MovPersonaje.cs
+++ Assets/scripts/MovPersonaje.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovPersonaje : MonoBehaviour
 {
+
+    public float multiplicador = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class MovPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float MovTeclas = Input.GetAxis("Horizontal"); //(a -1f - d 1f)
+
+        transform.position = new Vector3(
+            transform.position.x+MovTeclas*Time.deltaTime*multiplicador,
+            transform.position.y,
+            transform.position.z
+    
+        );
+
+        float miDeltaTime = Time.deltaTime;
+
+        //Debug.Log(Input.GetAxis("Horizontal"));
+
     }
 }
