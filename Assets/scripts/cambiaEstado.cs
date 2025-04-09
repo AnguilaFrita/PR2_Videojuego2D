c035d492fd295e874cb31a5a33d940d31abc6d07
“AnguilaFrita” <disconected15@gmail.com>
2025-04-09 14:05:31 +0200

    no entiendo

---
+++ Assets/scripts/cambiaEstado.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiaEstado : MonoBehaviour
+/****************************************************************************** no entiendo nara
+{
+
+    public GameObject miPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        miPanel = GameObject.Find("Panel")
+        miPanel.SetActive(false);
+    }
+
+    public void ActivaPanel()
+    {
+
+        if(miPanel.activeSelf == false){
+            miPanel.SetActive(true);
+        }
+
+          if(miPanel.activeSelf == true){
+            miPanel.SetActive(true);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
